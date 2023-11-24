27a2be27dd0ff831cb729a7bd006413706494289
Florian Pätzold <fpaetzold@uni-osnabrueck.de>
2023-11-24 14:40:49 +0100

    stop crawl everytime

--- myapp.py
+++ myapp.py
@@ -1,7 +1,15 @@
 from flask import Flask, request, render_template
 from crawler import Crawler
+import os
+from whoosh.index import open_dir
+import traceback
 
 app = Flask(__name__)
+indexdir = os.listdir("indexdir")
+
+@app.errorhandler(500)
+def internal_error(exception):
+   return "<pre>"+traceback.format_exc()+"</pre>"
 
 @app.route("/")
 def start():
@@ -13,6 +21,11 @@ def search():
         return "Something went wrong."
     else:
         query = request.args['input']
-        index = Crawler.crawl()
+        # if the index is built already, do not crawl again
+        if len(indexdir) == 0:
+            index = Crawler.crawl()
+        else:
+            index = open_dir("indexdir")
         results = Crawler.search(query, index)
+
         return render_template("results.html", results=results)
